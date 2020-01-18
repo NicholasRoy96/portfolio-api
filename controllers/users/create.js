fcc00462383e0c26f9a569701692c7fd553ab70f
Nick <nicholas.roy12@hotmail.co.uk>
2020-01-18 20:50:20 +0000

    Added linting

--- controllers/users/create.js
+++ controllers/users/create.js
@@ -1,14 +1,13 @@
 module.exports = async (req, res) => {
-    const { email, givenName, familyName } = req.body;
+  try {
+    const { email, givenName, familyName } = req.body
     if (!email || !givenName || !familyName) {
-        res.status(400).send('You must provide an email, given name, and family name.')
+      return res.status(400).send('You must provide an email, given name, and family name.')
     }
-
-    try {
-        const db = req.app.get('db')
-        const result = await db.collection('users').insertOne({ email, givenName, familyName, created: new Date().toString() });
-        if (result) res.status('201').send('User created');
-    } catch (err) {
-        res.status('500').send(err);
-    }
+    const db = req.app.get('db')
+    const result = await db.collection('users').insertOne({ email, givenName, familyName, created: new Date().toString() })
+    if (result) return res.status('201').send('User created')
+  } catch (err) {
+    return res.status('500').send(err)
+  }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
 
 // Create user routes
-router.post('/create', userController.createUser);
+router.post('/create', userController.createUser)
 
 // // Get user routes
-// router.get('/get/:id', userController.getUser);
-// router.get('/getAll', userController.getAllUsers);
+// router.get('/get/:id', userController.getUser)
+// router.get('/getAll', userController.getAllUsers)
 
 // // Update user routes
-// router.patch('/update/:id', userController.updateUser);
+// router.patch('/update/:id', userController.updateUser)
 
 // // Delete user routes
-// router.delete('delete/:id', userController.deleteUser);
+// router.delete('delete/:id', userController.deleteUser)
 
-module.exports = router;
+module.exports = router
