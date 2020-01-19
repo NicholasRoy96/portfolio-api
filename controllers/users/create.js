12ad96ca021ef064edd5f99f640c77746fcc42ee
Nick <nicholas.roy12@hotmail.co.uk>
2020-01-19 19:28:58 +0000

    Completed create user

--- controllers/users/create.js
+++ controllers/users/create.js
@@ -1,12 +1,33 @@
+const validator = require('email-validator')
+
 module.exports = async (req, res) => {
   try {
+    const db = req.app.get('db')
     const { email, givenName, familyName } = req.body
+
+    // existence checking required params
     if (!email || !givenName || !familyName) {
       return res.status(400).send('You must provide an email, given name, and family name.')
     }
-    const db = req.app.get('db')
-    const result = await db.collection('users').insertOne({ email, givenName, familyName, created: new Date().toString() })
-    if (result) return res.status('201').send('User created')
+
+    // email checks & inserting user
+    if (!validator.validate(email)) {
+      return res.status(400).send('You must provide a valid email')
+    }
+    const existingUser = await db.collection('users').findOne({ email: email.toLowerCase() })
+    if (!existingUser) {
+      const newUser = {
+        email: email.toLowerCase(),
+        givenName,
+        familyName,
+        created: new Date().toString()
+      }
+      await db.collection('users').insertOne(newUser)
+      return res.status('201').send('User created')
+    }
+    if (existingUser) {
+      return res.status('409').send('A user already exists with this email.')
+    }
   } catch (err) {
     return res.status('500').send(err)
   }
