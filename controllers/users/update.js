837a23d67fd5c11bc28487645f6e453958290e43
Nick <nicholas.roy12@hotmail.co.uk>
2020-01-20 17:26:16 +0000

    Added updateUser function

---
+++ controllers/users/update.js
@@ -0,0 +1,37 @@
+const mongo = require('mongodb')
+const validator = require('email-validator')
+
+module.exports = async (req, res) => {
+  try {
+    const db = req.app.get('db')
+    const { email, givenName, familyName } = req.body
+
+    // existence checking required params
+    if (!email || !givenName || !familyName) {
+      return res.status('400').send('You must provide an email, given name, and family name to update the user.')
+    }
+
+    // email checks
+    if (!validator.validate(email)) {
+      return res.status('400').send('You must provide a valid email')
+    }
+
+    // checking if user exists
+    const existingUser = await db.collection('users').findOne({ _id: mongo.ObjectID(req.params.id) })
+    if (!existingUser) {
+      return res.status('404').send("Cannot update a user that doesn't exist")
+    }
+
+    // updating user, keeping original created date
+    const updatedUser = {
+      email: email.toLowerCase(),
+      givenName,
+      familyName,
+      created: existingUser.created
+    }
+    await db.collection('users').findOneAndReplace({ _id: mongo.ObjectID(req.params.id) }, updatedUser)
+    return res.status('200').send('User updated')
+  } catch (err) {
+    return res.status('500').send(err)
+  }
+}
